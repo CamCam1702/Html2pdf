715a440aeceaf32699b73641d0e8c6d1b4447118
thanh123400 <themast3t@gmail.com>
2024-06-25 14:44:26 +0700

    ver 2.0

--- pdf/pdf.js
+++ pdf/pdf.js
@@ -8,10 +8,18 @@ window.onload = function () {
                 margin: 1,
                 filename: 'myfile.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 2},
+                html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-                
+                pagebreak: { mode: 'avoid-all', before: '#page2el' }
             };
-            html2pdf().from(allvoice).set(opt).save();
+        
+            html2pdf().from(allvoice).set(opt).toPdf().get('pdf').then(function (pdf) {
+                var totalPages = pdf.internal.getNumberOfPages();
+                console.log('Total pages: ' + totalPages); // Log ra số trang
+                for (var i = 1; i <= totalPages; i++) {
+                    pdf.setPage(i);
+                    pdf.text('Page ' + String(i) + ' of ' + String(totalPages), 10, pdf.internal.pageSize.height - 10);
+                }
+            }).save();
         })
 }
