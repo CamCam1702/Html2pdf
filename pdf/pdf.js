6a193730fc0975eb2ea29c5b4836afa929d6ef20
thanh123400 <themast3t@gmail.com>
2024-06-25 16:23:58 +0700

    Update pdf.js

--- pdf/pdf.js
+++ pdf/pdf.js
@@ -1,25 +1,33 @@
 window.onload = function () {
-    document.getElementById("download")
-        .addEventListener("click", () => {
-            const allvoice = this.document.getElementById("allvoice");
-            console.log(allvoice);
-            console.log(window);
-            var opt = {
-                margin: 1,
-                filename: 'myfile.pdf',
-                image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-                pagebreak: { mode: 'avoid-all', before: '#page2el' }
-            };
-        
-            html2pdf().from(allvoice).set(opt).toPdf().get('pdf').then(function (pdf) {
-                var totalPages = pdf.internal.getNumberOfPages();
-                console.log('Total pages: ' + totalPages); // Log ra số trang
-                for (var i = 1; i <= totalPages; i++) {
-                    pdf.setPage(i);
-                    pdf.text('Page ' + String(i) + ' of ' + String(totalPages), 10, pdf.internal.pageSize.height - 10);
-                }
-            }).save();
-        })
+    document.getElementById("download").addEventListener("click", () => {
+        const allvoice = document.getElementById("allvoice");
+
+        var opt = {
+            margin : [20,0],
+            filename: 'myfile.pdf',
+            image: { type: 'png', quality: 0.98 }, 
+            html2canvas: {
+                scale: 2,
+                logging: true,
+                dpi: 192,
+                letterRendering: true
+            },
+            jsPDF: {
+                unit: 'mm',
+                format: 'a4',
+                orientation: 'landscape',
+            },
+            pagebreak: {  mode: ['avoid-all', 'css', 'legacy'] }
+        };
+
+        html2pdf().from(allvoice).set(opt).toPdf().get('pdf').then(function(pdf) {
+            var totalPages = pdf.internal.getNumberOfPages();
+            for (i = 1; i <= totalPages; i++) {
+                pdf.setPage(i);
+                pdf.setFontSize(10);
+                pdf.setTextColor(100);
+                pdf.text('Page ' + i + ' of ' + totalPages, (pdf.internal.pageSize.getWidth() / 2.3), (pdf.internal.pageSize.getHeight() - 0.8));
+            }
+        }).save();
+    });
 }
