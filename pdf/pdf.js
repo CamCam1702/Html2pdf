3c74f2f91caa45a1d0aa0eecb9963486a478b7f8
thanh123400 <themast3t@gmail.com>
2024-07-01 11:45:50 +0700

    new

--- pdf/pdf.js
+++ pdf/pdf.js
@@ -1,33 +1,51 @@
 window.onload = function () {
-    document.getElementById("download").addEventListener("click", () => {
-        const allvoice = document.getElementById("allvoice");
-
-        var opt = {
-            margin : [20,0],
-            filename: 'myfile.pdf',
-            image: { type: 'png', quality: 0.98 }, 
-            html2canvas: {
-                scale: 2,
-                logging: true,
-                dpi: 192,
-                letterRendering: true
-            },
-            jsPDF: {
-                unit: 'mm',
-                format: 'a4',
-                orientation: 'landscape',
-            },
-            pagebreak: {  mode: ['avoid-all', 'css', 'legacy'] }
-        };
-
-        html2pdf().from(allvoice).set(opt).toPdf().get('pdf').then(function(pdf) {
-            var totalPages = pdf.internal.getNumberOfPages();
-            for (i = 1; i <= totalPages; i++) {
-                pdf.setPage(i);
-                pdf.setFontSize(10);
-                pdf.setTextColor(100);
-                pdf.text('Page ' + i + ' of ' + totalPages, (pdf.internal.pageSize.getWidth() / 2.3), (pdf.internal.pageSize.getHeight() - 0.8));
-            }
-        }).save();
+    document.getElementById("check").addEventListener('click', () => {
+        getSelectedSpan();
     });
+}
+
+function getSelectedText() {
+    var selection = window.getSelection();
+    return selection;
+}
+
+function highlightSelectedText() {
+    var selection = getSelectedText();
+    var selection_text = selection.toString();
+
+    var span = document.createElement('SPAN');
+    span.textContent = selection_text;
+    span.className = "highlight";
+
+    var range = selection.getRangeAt(0);
+    range.deleteContents();
+    // range.insertNode(span);
+}
+
+function getSelectedSpan() {
+    var selection = window.getSelection();
+    console.log(selection.rangeCount);
+    var range = selection.getRangeAt(0);
+    var startContainer = range.startContainer;
+
+    console.log(startContainer.parentElement);
+    console.log(startContainer.parentElement.textContent.length);
+    console.log(selection.toString().length);
+    console.log(findParentDivRecursive(startContainer));
+    var dady = findParentDivRecursive(startContainer);
+    console.log(dady.textContent.length);
+    
+    // return startContainer.parentElement.textContent.length;
+}
+
+function findParentDivRecursive(element) {
+    if (!element) {
+        return null;
+    }
+    
+    if (element.nodeName === 'DIV') {
+        return element;
+    } else {
+        return findParentDivRecursive(element.parentElement);
+    }
 }
